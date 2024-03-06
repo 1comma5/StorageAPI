1a370ebade6c8b479a9e68a3c77c2f7bfda085ec
1comma5 <stud130311@vyatsu.ru>
2024-03-06 17:12:37 +0300

    finally

--- StorageAPI/obj/Debug/net8.0/StorageAPI.AssemblyInfo.cs
+++ StorageAPI/obj/Debug/net8.0/StorageAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StorageAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de993c4362566ce95be6c481e45a341b59528b3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StorageAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StorageAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
